a648a054650ad864fe43538935201d2ca4f9d863
Cloud <michalcloud2003@gmail.com>
2025-05-17 02:01:15 +0200

    ✨ Defined Particle struct and ParticleType Enum to represent particle states and colours

--- Particle.hpp
+++ Particle.hpp
@@ -1,1 +1,16 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+
+//Defining the particles for the simulation
+enum class ParticleType {
+	Empty, //Empty Particle
+	Sand, //Sand Particle
+};
+
+//This Represents a single particle in the grid
+struct Particle {
+	//Default (empty)
+	ParticleType type = ParticleType::Empty;
+	//Default colour for the empty is black
+	sf::Color color = sf::Color::Black;
+};
